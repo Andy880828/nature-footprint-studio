d45ffcc2236994eb9668636634c3803618903cbe
ChingHsun Lin <andy0010502@gmail.com>
2025-02-13 13:49:09 +0800

    加入og:image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,13 +18,37 @@ export default defineNuxtConfig({
                         '一起探索自然的饋贈，提供生態專業服務，包括兩棲爬蟲、魚類、十足類、鳥類相關的生態課程，也提供生態調查及夜間導覽服務，歡迎進入網頁參訪',
                 },
                 {
-                    name: 'og:title',
+                    property: 'og:title',
                     content: '話蛇天足工作室',
                 },
                 {
-                    name: 'og:description',
+                    property: 'og:description',
                     content:
                         '一起探索自然的饋贈，提供生態專業服務，包括兩棲爬蟲、魚類、十足類、鳥類相關的生態課程，也提供生態調查及夜間導覽服務，歡迎進入網頁參訪',
+                },
+                {
+                    property: 'og:image',
+                    content: '/landing.webp',
+                },
+                {
+                    property: 'og:image:width',
+                    content: '1200',
+                },
+                {
+                    property: 'og:image:height',
+                    content: '630',
+                },
+                {
+                    property: 'og:type',
+                    content: 'website',
+                },
+                {
+                    name: 'twitter:card',
+                    content: 'summary_large_image',
+                },
+                {
+                    name: 'twitter:image',
+                    content: '/landing.webp',
                 },
             ],
         },
